7a23fd22e3c4238f720786d9cf20ce1c056f8478
Jolie Zhu <zhujolie973@gmail.com>
2026-08-27 10:22:09 -0400

    feat: add uoft webring

--- jolie-web-portfolio/app/components/header.tsx
+++ jolie-web-portfolio/app/components/header.tsx
@@ -55,6 +55,25 @@ export default function Header() {
                         />
                     </Button> */}
                 </Link>
+                <div className="flex items-center gap-2">
+                    <a
+                        href="https://uoftwebring.com/redirect?nav=prev&id=54"
+                        aria-label="Previous site in UofT Webring"
+                        >←</a
+                    >
+                    <a href="https://uoftwebring.com" target="_blank">
+                        <img
+                            src="https://uoftwebring.com/ring_logo.svg"
+                            alt="UofT Webring"
+                            className="w-6 h-auto"
+                        />
+                    </a>
+                    <a
+                        href="https://uoftwebring.com/redirect?nav=next&id=54"
+                        aria-label="Next site in UofT Webring"
+                        >→</a
+                    >
+                </div>
                 {/* <Button variant="outline" size="icon" aria-label="Switch mode">
                     <SunMoon/>
                 </Button> */}
